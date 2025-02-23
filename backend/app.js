def21e869962baa03dd4b180ada7a35bcba17557
Enrique <luis-enrique06@outlook.com>
2025-02-23 12:35:56 -0600

    program central error handler and update eslint config

--- backend/app.js
+++ backend/app.js
@@ -42,3 +42,16 @@ app.use('/', notFoundRouter);
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
+
+app.use((err, req, res, next) => {
+  // si un error no tiene estado, se muestra 500
+   const { statusCode = 500, message } = err;
+   res
+     .status(statusCode)
+     .send({
+      // comprueba el estado y muestra un mensaje basado en dicho estado
+       message: statusCode === 500
+         ? 'Se ha producido un error en el servidor'
+         : message
+     });
+ });
